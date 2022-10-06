cc6c4ae69e5f68a4c4cd158da3e28a6074eae7c1
Arnaud293 <arnaudlt293@gmail.com>
2022-10-06 16:18:24 +0200

    Add pages and routes

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>A propos</h1>
+            <br/>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique quas reiciendis eius inventore ratione ut impedit blanditiis, ex nam rerum placeat illum earum minus eum quasi at atque soluta necessitatibus fugiat alias ab totam? Natus repellendus sint praesentium rem, tempore quas iure quos sequi in veniam similique, magnam quibusdam ut ipsam eius commodi delectus laborum veritatis illum! Necessitatibus rem odio repudiandae porro obcaecati nisi ex accusantium eveniet nihil, modi saepe sequi dolor, corporis mollitia? Officia, vero. Eligendi, totam nisi qui dolor mollitia a, illum eius, possimus eum eveniet repellat? Et harum adipisci architecto nulla soluta modi blanditiis ipsum provident quos.</p>
+            <br />
+        </div>
+    );
+};
+
+export default About;
