bc7c02467ac15f44743087d6bee3ff760908f985
Arnaud293 <arnaudlt293@gmail.com>
2022-10-06 17:35:23 +0200

    Create logo draft, link with pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Logo from '../components/Logo';
 import Nav from '../components/Nav';
 
 const Home = () => {
     return (
         <div>
+            <Logo/>
             <Nav/>
             <h1>Accueil</h1>
         </div>
