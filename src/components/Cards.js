49582a1de78c867e512ead57389038db14a2746a
Arnaud293 <arnaudlt293@gmail.com>
2022-10-07 14:55:04 +0200

    Displaying dynamicly countries, population, capital, flags almost done

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,10 +3,11 @@ import React from 'react';
 const Cards = ({country}) => {
     return (
         <li className="card">
-            <img src={country.flags.png} alt="" />
+            <img src={country.flags.png} alt={"drapeau" + country.name.common} />
             <div className="infos">
                 <h2>{country.name.common}</h2>
-                
+                <h4>{country.capital}</h4>
+                <p>Pop : {country.population.toLocaleString()}</p>
             </div>
         </li>
     );
