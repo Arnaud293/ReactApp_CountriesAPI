f96aa55eda2c0244567619277ad176d677b4987f
Arnaud293 <arnaudlt293@gmail.com>
2022-10-07 12:16:14 +0200

    create Cards component for the Countries mapping

---
+++ src/components/Cards.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Cards = () => {
+    return (
+        <li className="cards">
+            <div className="infos">
+                <h2>Name</h2>
+            </div>
+        </li>
+    );
+};
+
+export default Cards;
