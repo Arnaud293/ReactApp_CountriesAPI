353519971469106a83f1bfa1579d3bb0062393e4
Arnaud293 <arnaudlt293@gmail.com>
2022-10-08 14:32:48 +0200

    Create first draft of logo page

---
+++ src/pages/Blog.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Nav from '../components/Nav';
+
+const Blog = () => {
+    return (
+        <div className='blog-container'>
+           <Logo />
+           <Nav />
+           <h1>Blog</h1>
+        </div>
+    );
+};
+
+export default Blog;
