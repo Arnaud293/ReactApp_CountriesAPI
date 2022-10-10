0c88c59e6863d850148b65118ce5141564ba8b9b
Arnaud293 <arnaudlt293@gmail.com>
2022-10-10 06:55:14 +0200

    add error message on the form

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../components/Logo';
 import Nav from '../components/Nav';
 
 const Blog = () => {
+
+    const [content, setContent] = useState('');
+    const [error, setError] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(content.length < 140){
+            setError(true);
+        } else {
+            setError(false);
+        }
+    }
+
     return (
         <div className='blog-container'>
            <Logo />
            <Nav />
            <h1>Blog</h1>
 
-           <form>
+           <form onSubmit={(e) => handleSubmit(e)}>
                 <input type="text" placeholder='Nom'/>
-                <textarea placeholder='Message'></textarea>
+                <textarea 
+                style={{border: error ? '1px solid red' : '1px solid #61dafb'}}
+                placeholder='Message' 
+                onChange={(e) => setContent(e.target.value)}></textarea>
+                { error && <p>Veuillez rédiger un message comprenant au minimum 140 caractères</p>}
                 <input type="submit" value="Envoyer" />
            </form>
            <ul></ul>
