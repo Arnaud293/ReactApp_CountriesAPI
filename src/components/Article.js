6b1580da46157d93fa515c00c991d3e7e2e28617
Arnaud293 <arnaudlt293@gmail.com>
2022-10-10 09:55:59 +0200

    Axios put, possibility to update a post

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 
 const Article = ({article}) => {
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [editContent, setEditContent] = useState('');
 
     const dateFormater = (date) => {
         let newDate = new Date(date).toLocaleDateString('fr-FR', {
@@ -13,17 +17,34 @@ const Article = ({article}) => {
         return newDate;
     }
 
+    const handleEdit = () => {
+
+        const data = {
+            author: article.author,
+            content : editContent ? editContent : article.content,
+            date: article.date,
+        }
+
+        axios.put('http://localhost:3004/articles/' + article.id, data).then(() => {setIsEditing(false);})
+        
+    }
+
     return (
         <div className='article'>
             <div className="card-header">
                 <h3>{article.author}</h3>
                 <em>Posté le : {dateFormater(article.date)}</em>
             </div>
-            <p>
-                {article.content}
-            </p>
+            {
+                isEditing ? 
+                <textarea defaultValue={article.content} onChange={(e) => setEditContent(e.target.value)}></textarea> 
+                :<p>{editContent ? editContent : article.content}</p>    
+            }
             <div className="btn-container">
-                <button>Editer</button>
+                {isEditing ? <button onClick={() => handleEdit()}>Valider</button>
+                 : 
+                (<button onClick={() => setIsEditing(true)}>Editer</button>)}
+                
                 <button>Supprimer</button>
             </div>
         </div>
